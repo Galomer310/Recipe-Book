9a1cc016e91fc3549d5605121a85c0aa8491fbb2
Gal Omer <galomer6708@gmail.com>
2025-01-09 20:41:37 +0200

    second commit

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/Recipe-Book/",
   build: {
-    outDir: "dist", // Set the output directory
-    assetsDir: "assets", // Set the assets directory where CSS and JS files will go
+    outDir: "dist",
+    assetsDir: "assets",
     rollupOptions: {
-      input: "index.html", // Ensure Vite is bundling the correct HTML file
+      input: "index.html",
     },
   },
 });
